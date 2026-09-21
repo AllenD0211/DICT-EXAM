5d71d3e1ecadabfe6d06088baa20a562a9ae5593
allendave. <allendavedeala@gmail.com>
2026-09-22 00:02:55 +0800

    string

---
+++ Data Manipulation/Datamanipulation.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+public class Datamanipulation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter you full name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter course: ");
+        String course = sc.nextLine();
+
+        System.out.print("Enter ID: ");
+        int id = sc.nextInt();
+        
+        System.out.println("");
+        System.out.println(name.toUpperCase());
+        System.out.println(course.toLowerCase());
+        System.out.println(id);
+
+
+
+    }
+}
