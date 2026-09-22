563466d903422b0353b81c7e394b2dbfe0682e9d
allendave. <allendavedeala@gmail.com>
2026-09-23 00:03:21 +0800

    update

--- Data Manipulation/Datamanipulation.java
+++ Data Manipulation/Datamanipulation.java
@@ -14,13 +14,59 @@ public class Datamanipulation {
         System.out.print("Enter ID: ");
         int id = sc.nextInt();
         
+        System.out.println("=== STUDENT INFORMATION ===");
         System.out.println("");
-        System.out.println(name.toUpperCase());
-        System.out.println(course.toLowerCase());
-        System.out.println(name.length());
-        System.out.println(name.indexOf(name));
+        
+        System.out.println("Original Name: " + name );
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+        System.out.println("Name Lenght: " + name.length());
 
-        if 
+        System.out.println("First Letter: " + name.charAt(0));
+        for( int i = name.length() -1; i>=0; i--){
+            System.out.println("Last Name: "+ name.charAt(i));
+            break;
+        }
+
+        System.out.println("");
+
+        if (name.toUpperCase().contains("A")){
+            System.out.println("Contains 'A'? : Yes ");
+        }else{
+            System.out.println("Contains 'A'? : No");
+        }
+        
+        System.out.print("First name: ");
+        for(int i = 0; i < name.length(); i++ ){
+            System.out.print(name.charAt(i));
+            if (name.charAt(i) == ' ') {
+           break;
+           }
+        }
+        System.out.println(" ");
+        
+        
+        
+        int n = 0;
+        for(int i = 0; i < name.length(); i++){
+            
+            if (name.charAt(i) == ' ') {
+               n++; 
+            }
+        }
+        System.out.println("Spaces: " + n);
+        
+
+        System.out.println("  ");
+
+        System.out.println("Course: "+ course);
+        System.out.println("Student ID: " +id);
+       
+       
+
+
+
+         
       
 
 
---
+++ Data Manipulation/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+for(int i = 0; i < name.length(); i++){
+        //     if (name.charAt(i) == ' ') {
+        //         System.out.println(i);
+        //     }
+
+        // }
